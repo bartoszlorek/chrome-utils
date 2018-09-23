8e12829304520df3a8bc17e8d7dbab7e3f8b007a
Bartosz Lorek <bery.lorek@gmail.com>
2018-09-23 18:01:14 +0200

    add more tests

--- src/executable-tab.spec.js
+++ src/executable-tab.spec.js
@@ -13,6 +13,14 @@ global.chrome = {
     }
 }
 
+const silently = () => {/*o_O*/}
+const errorMessage = 'error_value'
+const forceError = () => {
+    global.chrome.runtime.lastError = {
+        message: errorMessage
+    }
+}
+
 describe('exec()', () => {
     beforeEach(() => {
         global.chrome.tabs.executeScript.mockClear()
@@ -21,8 +29,8 @@ describe('exec()', () => {
 
     const tabId = 10
     const tab = { id: tabId }
+    const tabWithUrl = { id: tabId, url: 'path_url'}
     const defaultArgs = [tabId, VOID, expect.any(Function)]
-    const errorMessage = 'error'
 
     it('should reject to incorrect tab', async () => {
         await expect(exec()).rejects.toBe(INCORRECT)
@@ -30,21 +38,17 @@ describe('exec()', () => {
         await expect(exec({ id: -1 })).rejects.toBe(INCORRECT)
     })
 
-    it('should reject with error message', async () => {
-        global.chrome.runtime.lastError = {
-            message: errorMessage
-        }
-        await expect(exec(tab)).rejects.toBe(errorMessage)
-        expect(global.chrome.tabs.executeScript).toHaveBeenLastCalledWith(
-            ...defaultArgs
-        )
-    })
-
     it('should resolve with tab id', async () => {
         await expect(exec(tab)).resolves.toBe(tabId)
-        expect(global.chrome.tabs.executeScript).toHaveBeenLastCalledWith(
-            ...defaultArgs
-        )
+        expect(global.chrome.tabs.executeScript)
+            .toHaveBeenLastCalledWith(...defaultArgs)
+    })
+
+    it('should reject with error message', async () => {
+        forceError()
+        await expect(exec(tab)).rejects.toBe(errorMessage)
+        expect(global.chrome.tabs.executeScript)
+            .toHaveBeenLastCalledWith(...defaultArgs)
     })
 
     it('should memoize correct value', async () => {
@@ -53,8 +57,38 @@ describe('exec()', () => {
         expect(memo).toEqual({ '10': CORRECT })
     })
 
-    it('should memoize incorrect value', async () => {})
-    it('should return memoized correct value', async () => {})
-    it('should return memoized incorrect value', async () => {})
-    it('should use hostname as a memo prop', async () => {})
+    it('should memoize incorrect value', async () => {
+        forceError()
+        const memo = {}
+        await exec(tab, memo).catch(silently)
+        expect(memo).toEqual({ '10': errorMessage })
+    })
+
+    it('should return memoized correct value', async () => {
+        const memo = {}
+        await exec(tab, memo)
+        await exec(tab, memo)
+        expect(global.chrome.tabs.executeScript)
+            .toHaveBeenCalledTimes(1)
+    })
+
+    it('should return memoized incorrect value', async () => {
+        forceError()
+        const memo = {}
+        await exec(tab, memo).catch(silently)
+        await exec(tab, memo).catch(silently)
+        expect(global.chrome.tabs.executeScript)
+            .toHaveBeenCalledTimes(1)
+    })
+
+    it('should use hostname as a memo prop', async () => {
+        const memo = {}
+        const hostname = 'hostname_url'
+        document.createElement = jest.fn(() => ({
+            href: null,
+            hostname
+        }))
+        await exec(tabWithUrl, memo)
+        expect(memo).toEqual({ [hostname]: CORRECT })
+    })
 })
